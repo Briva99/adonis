16ca07e0036c03b153972685328d6a516d022d47
briva bagus <briva.coding@gmail.com>
2021-11-22 16:58:41 +0700

    CRUD Adonis

---
+++ start/routes.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const PostController = require('../app/Controllers/Http/PostController')
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.on('/').render('welcome')
+Route.get('/posts','PostController.index').as('posts.index')
+Route.post('/posts/store','PostController.store').as('posts.store')
+Route.get('/posts/create','PostController.create').as('posts.create')
+Route.get('posts/edit/:id', 'PostController.edit').as('posts.edit')
+Route.post('/posts/update/:id', 'PostController.update').as('posts.update')
+Route.get('posts/delete/:id', 'PostController.delete').as('posts.delete')
